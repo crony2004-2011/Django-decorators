9c703fd398f876294334cb3437e3b84aa91785f5
AS <77235804+crony2004-2011@users.noreply.github.com>
2021-12-07 14:55:08 +0530

    Add files via upload
    Decorators have one main function and two other functions within it 1> wrapper_func  2> decorator
    Inside the decorator function, all the logic is added.
    (view_func) is the parameter or the function that is to be passed through the decorator function or the function on which the permission is supposed to be created.

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from pyatspi import document
+
+from . import views
+from django.conf import *
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name="list"),
+    path('register/', views.register, name='register'),
+    path('confidential/', views.admin, name='admin'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
